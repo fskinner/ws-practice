1ebc1d740e311876923cda9f2145ef7a212ef7c1
Felipe Skinner <felipeskinner@gmail.com>
2015-04-14 22:07:25 -0300

    Added WIP message

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+	function showModal(){
+		document.getElementById("modal").className = "modal-layer show-modal";
+	}
+
+	function hideModal(){
+		document.getElementById("modal").className = "modal-layer";
+	}
+
+	var menuOptions = document.getElementsByClassName("modal-trigger");
+
+	for(var i = 0; i < menuOptions.length; i++){
+		menuOptions[i].addEventListener("click", showModal);
+	}
+
+	document.getElementById("modal-close").addEventListener("click", hideModal);
+})();
